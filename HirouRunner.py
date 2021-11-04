e7fca09b8b0e640ad21cdfe03db456d52805c4b8
Jecta <68130184+Jecta@users.noreply.github.com>
2021-11-04 19:21:44 +0100

    Release
    Simple program to recieve system specifications, now compatible on windows, macos and linux.

--- HirouRunner.py
+++ HirouRunner.py
@@ -1,1 +1,43 @@
+import platform
 
+# Time for windows.
+import ctypes
+
+lib = ctypes.windll.kernel32
+
+t = lib.GetTickCount64()
+
+t = int(str(t)[:-3])
+
+mins, sec = divmod(t, 60)
+hour, mins = divmod(mins, 60)
+days, hour = divmod(hour, 24)
+
+# Time for macos and linux
+import os
+from os import system
+
+clearConsole = lambda: os.system("cls" if os.name in ("nt", "dos") else "clear")
+
+t = os.popen("uptime -p").read()[:-1]
+
+import ctypes
+
+clearConsole()
+
+# Hirou
+ctypes.windll.kernel32.SetConsoleTitleW("意味")
+print("-" * 5, "H1R0U", "-" * 5)
+uname = platform.uname()
+print(f"System: {uname.system}")
+print(f"Machine: {uname.machine}")
+print(f"System-Name: {uname.node}")
+print(f"System-OS: {platform.system()}")
+print(f"Processor: {uname.processor}")
+print(f"Architecture: {platform.architecture()}")
+print(f"Python-Version: {platform.python_version()}")
+print(f"System-Uptime: {days} days, {hour:02}:{mins:02}:{sec:02}")
+print(t)
+print(f"Made by https://github.com/jecta")
+
+k = input("")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Hirou
 Wide-compatible, lightweight neofetch alternative written in py.
+##### Written in and for windows, will most likely still work on all other operating systems relevant today...
